b790d5fc1b3920f5e303bab1d510d91c6c3323d3
Daniel Michaeloudis <dmichaeloudis@gmail.com>
2025-09-07 23:04:49 +0100

    done stuffs

--- BrainFuckInterpreter.cpp
+++ BrainFuckInterpreter.cpp
@@ -1,11 +1,54 @@
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+const int MEM_SIZE = 30000;
+
+void interpretLoop(std::ifstream *CodeFile)
+{
+    unsigned char *bfMem[MEM_SIZE];
+    int bfMemPtr = 0;
+    char nextCmd;
+    std::vector<int> loops;
+    while (CodeFile->get(nextCmd))
+    {
+        std::cout << "char: " << nextCmd << "\n";
+        switch (nextCmd)
+        {
+        case '>':
+            bfMemPtr++;
+            if (bfMemPtr >= MEM_SIZE)
+                bfMemPtr = 0;
+            break;
+        case '<':
+            bfMemPtr--;
+            if (bfMemPtr < 0)
+                bfMemPtr = MEM_SIZE - 1;
+            break;
+        case '+':
+            bfMem[bfMemPtr]++;
+            break;
+        case '-':
+            bfMem[bfMemPtr]--;
+            break;
+        case '.':
+            std::cout << (int)bfMem[bfMemPtr];
+            break;
+        case ',':
+            std::cin >> bfMem[bfMemPtr];
+        };
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
         throw "Invalid Arguments";
     };
-    std::string file = argv[1];
-    unsigned char *bfMem[3000];
+    std::string filePath = argv[1];
+    std::ifstream CodeFile(filePath);
+    interpretLoop(&CodeFile);
     return 0;
 }
